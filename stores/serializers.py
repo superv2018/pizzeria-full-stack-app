9b0f03e954a33d953d21a97b2ade6a21465bdcd8
Victor <pelumiv@yahoo.com>
2023-01-14 19:55:51 +0200

    add post data and route

---
+++ stores/serializers.py
@@ -0,0 +1,36 @@
+from rest_framework import serializers
+from .models import Pizzeria
+from rest_framework.reverse import reverse
+
+class PizzeriaListSerializer(serializers.ModelSerializer):
+    absolute_url = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Pizzeria
+        fields = [
+            'id',
+            'pizzeria_name',
+            'city',
+            'zip_code',
+            'absolute_url',
+        ]
+
+    def get_absolute_url(self, obj):
+        return reverse('pizzeria_detail', args=(obj.pk,))
+
+class PizzeriaDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pizzeria
+        fields = [
+            'id',
+            'pizzeria_name',
+            'street',
+            'city',
+            'zip_code',
+            'website',
+            'phone_number',
+            'description',
+            'logo_image',
+            'email',
+            'active'
+        ]
